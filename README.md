99aa1c419d2b532c9d70e54fceb1b85c9b16571a
gabbybamps <gabbybamps@gmail.com>
2021-10-07 01:23:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This React App allows users to add tasks to a list. With the use of a custom hook, functionalities for getting and posting data to a Firebase Realtime database is handled.
+When a new task is entered and sent, the task list is updated both on the webpage and also in the database.
+Some concepts in use are: building custom hooks, configuring and using custom hooks in more components etc.
---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import useHttp from "./hooks/use-http";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const transformTasks = (tasksObj) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  };
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    fetchTasks(
+      {
+        url: "https://react-is-reactive-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
